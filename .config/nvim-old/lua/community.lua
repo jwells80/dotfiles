1122183d90fae439a0922c198a53b220ada7c52c
Jason Wells <7544731+jwells80@users.noreply.github.com>
2024-07-25 07:20:04 -0500

    move nvim

---
+++ .config/nvim-old/lua/community.lua
@@ -0,0 +1,15 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.pack.python-ruff" },
+}
